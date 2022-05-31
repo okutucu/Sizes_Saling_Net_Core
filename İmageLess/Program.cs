7469e9539774137228508eedb3c52b71b6877152
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-05-31 18:00:39 +0300

    Add project files.

---
+++ İmageLess/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace İmageLess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string picturesPath = @"C:\Users\silve\Desktop\images";
+
+            var files = Directory.GetFiles(picturesPath);
+
+            Parallel.ForEach(files, (item) =>
+            {
+                Console.WriteLine("Thread no : " + Thread.CurrentThread.ManagedThreadId);
+
+                Image img = new Bitmap(item);
+                var thumbnail = img.GetThumbnailImage(300,300,()=> false,IntPtr.Zero);
+
+                thumbnail.Save(Path.Combine(picturesPath, "lessimages",Path.GetFileName(item)));
+            });
+        }
+    }
+}
